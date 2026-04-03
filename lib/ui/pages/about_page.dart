7d2e531da38c20ba14ae7df34c2cd7ec2b1fded0
Abdullah Zeb <abdullahzeb1919@gmail.com>
2026-04-03 11:21:35 +0500

    V1.0.4 (Updates File Names, and shared prefernaces directory)

--- lib/ui/pages/about_page.dart
+++ lib/ui/pages/about_page.dart
@@ -61,11 +61,11 @@ class AboutPage extends StatelessWidget {
                     ),
                     const Divider(height: 30, color: Colors.white10),
                     
-                    _infoRow("App Version", "1.0.3"),
-                    _infoRow("Version Release Date", "1/4/2026"),
+                    _infoRow("App Version", "1.0.4"),
+                    _infoRow("Version Release Date", "3/4/2026"),
                     _infoRow("Build Type", "Release (Windows)"),
                     _infoRow("Latest Patch", "Patch #0"),
-                    _infoRow("Patch Release Date", "2/4/2026"),
+                    _infoRow("Patch Release Date", "3/4/2026"),
 
                     
                     const SizedBox(height: 20),
